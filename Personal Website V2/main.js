b9d651e90e2635079cf41548b46c546c448e435c
DMENNI02 <48967552+DMENNI02@users.noreply.github.com>
2021-03-02 15:53:08 -0500

    initial upload

---
+++ Personal Website V2/main.js
@@ -0,0 +1,10 @@
+// Javascript for Devin's site
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
